b233eb735168e0190211ffd8e837fb4dd7a72c56
Sumit Khandelwal <sumitkhandelwal@Sumits-MacBook-Air.local>
2023-06-05 19:55:36 +0530

    destructuring

--- es6-basics/app.js
+++ es6-basics/app.js
@@ -78,24 +78,133 @@
 // doSomething("john@test", 32);
 // doSomething("john@test", 32, true);
 
-let arr = [3, 4, 5];
+// let arr = [3, 4, 5];
 
-let moreNumbers = [1, 2, ...arr];
+// let moreNumbers = [1, 2, ...arr];
 
-console.log(moreNumbers);
+// console.log(moreNumbers);
 
-let userOne = {
-  email: "john@test",
-  age: 32,
-};
+// let userOne = {
+//   email: "john@test",
+//   age: 32,
+// };
 
-let userTwo = {
-  ...userOne,
-  age: 38,
-};
+// let userTwo = {
+//   ...userOne,
+//   age: 38,
+// };
 
-console.log(userTwo); // { email, age }
+// console.log(userTwo); // { email, age }
 
-let result = Object.assign({}, { email: "jenny@test" }, { age: 40 });
+// let result = Object.assign({}, { email: "jenny@test" }, { age: 40 });
 
-console.log("RESULT : ", result);
+// console.log("RESULT : ", result);
+
+// BLOCK SCOPE
+
+// function doSomething(arr) {
+//   if (arr.length > 2) {
+//     let LOAD = "LOADING";
+//     console.log(FLASH); // ?
+//   } else {
+//     let FLASH = "FLASHING";
+//   }
+// }
+
+// doSomething([2, 3, 4, 5]);
+
+// const username = "John Doe";
+
+// const age = 30;
+
+// age = 32;
+
+// const user = {
+//   name: "John",
+// };
+
+// user = {
+//   name: "Jenny",
+// };
+
+// // user.name = "Jenny";
+
+// console.log(user); // ?
+
+// const fruits = ["Apple", "Banana", "Kiwi"];
+
+// fruits.push("Oranges");
+
+// console.log(fruits); // ?
+
+// fruits = ["Apple", "Banana", "Kiwi", "Guava"];
+
+// DESTRUCTURING
+
+// let friends = ["Joe", "Monica", "Chandler", "Ross"];
+
+// let [f1, , f3, f4, f5] = friends;
+
+// console.log(f3); // ?
+
+// console.log(f5); // undefined
+
+// let user = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 32,
+// };
+
+// let { lastName: ln, age, firstName: fn } = user;
+
+// console.log(ln, age, fn);
+
+// let user = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 32,
+//   friends: ["Joe", "Monica", "Ross"],
+//   address: {
+//     street: "Baker Street",
+//     city: "London",
+//   },
+// };
+
+// let {
+//   firstName,
+//   lastName,
+//   age,
+//   friends: [f1, f2, f3],
+//   address: { street: st, city: ct },
+// } = user;
+
+// console.log(ct, f2, firstName);
+
+let users = [
+  { email: "john@test", age: 32 },
+  { email: "jenny@test", age: 34 },
+  { email: "jack@test", age: 36 },
+];
+
+// let [{ email: e1, age: a1 }, { email: e2, age: a2 }, { email: e3, age: a3 }] =
+//   users;
+
+// a1 = 44;
+
+// console.log(e2, a3);
+
+// console.log(users); // ?
+
+let [u1, u2, u3] = users;
+
+u1.age = 40;
+
+console.log(users); // ?
+
+// Deep Copy
+const clonedCopy = JSON.parse(JSON.stringify(users));
+
+clonedCopy[0].age = 21;
+
+console.log("ORIGINAL : ", users);
+console.log("CLONED : ", clonedCopy);
